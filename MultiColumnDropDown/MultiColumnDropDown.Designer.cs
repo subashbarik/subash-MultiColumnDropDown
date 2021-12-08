0720c0494357b018cb963b51bebd5582e8f1c95f
subash <subash.barik@gmail.com>
2021-12-08 08:14:30 +0530

    Initial Commit

---
+++ MultiColumnDropDown/MultiColumnDropDown.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace MultiColumnDropDown
+{
+    partial class MultiColumnDropDown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMCD = new System.Windows.Forms.Button();
+            this.txtMCD = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMCD
+            // 
+            this.btnMCD.Location = new System.Drawing.Point(199, 15);
+            this.btnMCD.Name = "btnMCD";
+            this.btnMCD.Size = new System.Drawing.Size(29, 22);
+            this.btnMCD.TabIndex = 1;
+            this.btnMCD.UseVisualStyleBackColor = true;
+            // 
+            // txtMCD
+            // 
+            this.txtMCD.Location = new System.Drawing.Point(36, 15);
+            this.txtMCD.Name = "txtMCD";
+            this.txtMCD.Size = new System.Drawing.Size(166, 22);
+            this.txtMCD.TabIndex = 2;
+            // 
+            // MultiColumnDropDown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtMCD);
+            this.Controls.Add(this.btnMCD);
+            this.Name = "MultiColumnDropDown";
+            this.Size = new System.Drawing.Size(256, 55);
+            this.Load += new System.EventHandler(this.MultiColumnDropDown_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMCD;
+        private System.Windows.Forms.TextBox txtMCD;
+    }
+}
---
+++ MultiColumnDropDown/MultiColumnDropDown.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiColumnDropDown
+{
+    public partial class MultiColumnDropDown: UserControl
+    {
+        public DataGridView gridMCD { get; set; }
+        public bool bGridCreated { get; set; } = false;
+        public GridViewMCD objGVM = null;
+        public MultiColumnDropDown()
+        {
+            InitializeComponent();
+        }
+
+        private void MultiColumnDropDown_Load(object sender, EventArgs e)
+        {
+            if(!this.bGridCreated)
+            {
+                objGVM = new GridViewMCD();
+                this.gridMCD = objGVM.gridMCD;
+                this.bGridCreated = objGVM.bGridCreated;
+                this.Controls.Add(this.gridMCD); // add the grid view control to the form
+            }
+            
+        }
+        // default code to create a gridview can be modified in the form level
+        public virtual void CreateGV()
+        {
+            // Create a Grid View in the constructor
+            if (!this.bGridCreated)
+            {
+                gridMCD = new DataGridView();
+                gridMCD.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                gridMCD.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+                //Header column cell style
+                System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+                dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+                dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+                dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+                dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+                dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+                dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+                gridMCD.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+                gridMCD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+
+                System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+                dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                gridMCD.RowsDefaultCellStyle = dataGridViewCellStyle2;
+
+                // Grid events should be assiged once , hence code is here
+                // This should be handled at the form level where it will be used
+                /* gridMCD.Leave += new EventHandler(dgvProduct_Leave);
+                 gridMCD.KeyDown += new KeyEventHandler(dgvProduct_KeyDown);
+                 gridMCD.CellClick += new DataGridViewCellEventHandler(dgvProduct_CellClick);*/
+
+                gridMCD.ReadOnly = true;
+                //groupBox2.Controls.Add(dgvProduct);
+                //this.Controls.Add(dgvProduct);
+                gridMCD.Visible = false;
+
+                this.bGridCreated = true;
+                // Below code should be handled at the form level where it will be used
+                /*
+                dgvProduct.DataSource = this.dtAvailableProducts;
+                hideColumnsProductGrid();
+                //setProductGridHeader();
+                setProductGridColumnWidthProduct();
+                dgvProduct.Refresh();
+                dgvProduct.Update();
+                dgvProduct.ClearSelection();
+                reloadGrid("");
+                */
+            }
+        }
+        // Default code to draw an area where gird view will be displayed
+        // should be implemented in the form level
+        public virtual void ShowGV()
+        {
+            this.gridMCD.Visible = true;
+            this.gridMCD.AllowUserToAddRows = false;
+            this.gridMCD.ClearSelection();
+        }
+
+        // Default code to hide gridview
+        // should be implemented in the form level
+        public virtual void HideGV()
+        {
+            this.gridMCD.Visible = false;
+        }
+
+    }
+}
